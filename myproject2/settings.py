889a7084a0d0a1310d7e798d8c19a5437fcd6391
AndreySivyakov <solgas80@gmail.com>
2019-12-25 13:37:11 -0700

    test whitenoise

--- myproject2/settings.py
+++ myproject2/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myproject2.urls'
@@ -163,3 +164,4 @@ STATIC_URL = '/static/'
 #    os.path.join(BASE_DIR, 'static'),
 #)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
